926e61bb5041cdc8f2936b1ec5eaf6db4bc2a124
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-05-17 15:21:18 +0530

    Basic Schema created

---
+++ App/students.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema ;
+
+const StudentSchema = new Schema ({
+    name : String 
+});
+
+const Student = mongoose.model("student",StudentSchema);
+
+//export
+module.exports = Student ;
