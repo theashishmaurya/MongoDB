4abc9a96431a42eef003e638b29a14fb8f550133
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-05-18 07:34:26 +0530

    used mocha hooks

--- Test/mo_helper_test.js
+++ Test/mo_helper_test.js
@@ -1,12 +1,21 @@
 const mongoose = require ('mongoose');
 
+// Using mocha's Hooks
+
+before((done)=>{
 //connect mongoose and mongodb
 mongoose.connect("mongodb://localhost/mongotube",{useNewUrlParser: true});
 
 //setting up promise on the connection
 
 mongoose.connection
-    .once("open" , () => console.log("Connected"))
+    .once("open" , () => {
+        // console.log("Connected")
+        done();
+    })
     .on("error" , error => {
         console.log("Your Error : " , error)
     });
+
+});
+
